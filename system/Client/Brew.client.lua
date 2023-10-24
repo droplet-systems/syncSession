4f48384eef0415e7fd1b996d8db9ed7a8436b72b
michaelrbx <startreksrbx@gmail.com>
2023-10-24 19:18:41 +0100

    styling

--- system/Client/Brew.client.lua
+++ system/Client/Brew.client.lua
@@ -1,7 +1,7 @@
-local Players = game:GetService('Players')
+local Players = game:GetService("Players")
 local Player = Players.LocalPlayer
 
-local ReplicatedStorage = game:GetService('ReplicatedStorage')
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Shared = ReplicatedStorage.syncSession
 
 local warn = require(Shared.Warn)
@@ -11,38 +11,38 @@ local SendSession = Remotes.SendSession
 local FilterText = Remotes.FilterText
 
 local Months = {
-    [1] = 'January',
-    [2] = 'Feburary',
-    [3] = 'March',
-    [4] = 'April',
-    [5] = 'May',
-    [6] = 'June',
-    [7] = 'July',
-    [8] = 'August',
-    [9] = 'September',
-    [10] = 'October',
-    [11] = 'November',
-    [12] = 'December'
+	[1] = "January",
+	[2] = "Feburary",
+	[3] = "March",
+	[4] = "April",
+	[5] = "May",
+	[6] = "June",
+	[7] = "July",
+	[8] = "August",
+	[9] = "September",
+	[10] = "October",
+	[11] = "November",
+	[12] = "December",
 }
 
 SendSession.OnClientEvent:Connect(function(Information, Text, Time)
-    for _, Object in pairs(Information:GetChildren()) do
-        if Object:IsA('Frame') then
-            local FilteredText = FilterText:InvokeServer(Object.Text.Text)
-            Object.Text.Text = FilteredText
-        end
-    end
+	for _, Object in pairs(Information:GetChildren()) do
+		if Object:IsA("Frame") then
+			local FilteredText = FilterText:InvokeServer(Object.Text.Text)
+			Object.Text.Text = FilteredText
+		end
+	end
 
-    print('Ico', Time)
-    Time = DateTime.fromIsoDate(Time):ToLocalTime()
-    print('Local', Time)
+	print("Ico", Time)
+	Time = DateTime.fromIsoDate(Time):ToLocalTime()
+	print("Local", Time)
 
-    local Minute = Time.Minute
-    if Minute == 0 then
-        Minute = '00'
-    end
+	local Minute = Time.Minute
+	if Minute == 0 then
+		Minute = "00"
+	end
 
-    Text.Text = string.format('%s %s\n%s:%s', Time.Day, Months[Time.Month], Time.Hour, Minute)
+	Text.Text = string.format("%s %s\n%s:%s", Time.Day, Months[Time.Month], Time.Hour, Minute)
 end)
 
 -- os.date('%B', Time.Month)
