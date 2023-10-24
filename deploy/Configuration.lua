2eebd9123c4536764a4984b5b84414afbda06318
michaelrbx <startreksrbx@gmail.com>
2023-10-24 19:17:29 +0100

    New source code ID & configurations

--- deploy/Configuration.lua
+++ deploy/Configuration.lua
@@ -4,98 +4,100 @@
 -- ██║░░██║██╔══██╗██║░░██║██╔═══╝░██║░░░░░██╔══╝░░░░░██║░░░
 -- ██████╔╝██║░░██║╚█████╔╝██║░░░░░███████╗███████╗░░░██║░░░
 -- ╚═════╝░╚═╝░░╚═╝░╚════╝░╚═╝░░░░░╚══════╝╚══════╝░░░╚═╝░░░
+-- Credits: @metricsrbx (Contributor) & @Nicklaus-s (Creator) 
 -- Refer to our documentation to set up our product properly: https://droplet-systems.gitbook.io/syncsession
 
-local Players = game:GetService('Players')
+local Players = game:GetService("Players")
 
 return {
-    Key = '1a2b3c4d5e6f7g8h9i0j',
-    Token = '1a2b3c4d5e6f7g8h9i0j1a2b3c4d5e6f7g8h9i0j',
-    BoardName = 'Sessions',
+	Key = "",
+	Token = "",
+	BoardName = "Sessions",
 
-    List = 'Upcoming',
+	List = "Upcoming",
+	Labels = false,
 
-    Fields = {
-        {
-            Argument = 'Host',
+	Fields = {
+		{
+			Argument = "Host",
 
-            Image = nil,
-            ImageColor = Color3.new(255, 255, 255),
-            GetImage = function(Value)
-                local Status, Identifier = pcall(function()
-                    return Players:GetUserIdFromNameAsync(Value)
-                end)
+			Image = nil,
+			ImageColor = Color3.new(255, 255, 255),
+			GetImage = function(Value)
+				local Status, Identifier = pcall(function()
+					return Players:GetUserIdFromNameAsync(Value)
+				end)
 
-                if not Status then
-                    Identifier = nil
-                end
+				if not Status then
+					Identifier = nil
+				end
 
-                if Identifier then
-                    return string.format('rbxthumb://type=AvatarHeadShot&id=%s&w=420&h=420', Identifier)
-                else
-                    return 'rbxassetid://7072717759'
-                end
-            end,
+				if Identifier then
+					return string.format("rbxthumb://type=AvatarHeadShot&id=%s&w=420&h=420", Identifier)
+				else
+					return "rbxassetid://7072717759"
+				end
+			end,
 
-            Value = '<b>%s</b> is hosting.',
-            GetValue = function(Value)
-                local Status, Result = pcall(function()
-                    return Players:GetUserIdFromNameAsync(Value)
-                end)
+			Value = "<b>%s</b> is hosting.",
+			GetValue = function(Value)
+				local Status, Result = pcall(function()
+					return Players:GetUserIdFromNameAsync(Value)
+				end)
 
-                if not Status then
-                    Result = nil
-                end
+				if not Status then
+					Result = nil
+				end
 
-                if Result then
-                    return Value
-                else
-                    return 'Unknown user'
-                end
-            end
-        },
+				if Result then
+					return Value
+				else
+					return "Unknown user"
+				end
+			end,
+		},
 
-        {
-            Argument = 'Co-Host',
+		{
+			Argument = "Co-Host",
 
-            Image = nil,
-            ImageColor = Color3.new(255, 255, 255),
-            GetImage = function(Value)
-                local Status, Identifier = pcall(function()
-                    return Players:GetUserIdFromNameAsync(Value)
-                end)
+			Image = nil,
+			ImageColor = Color3.new(255, 255, 255),
+			GetImage = function(Value)
+				local Status, Identifier = pcall(function()
+					return Players:GetUserIdFromNameAsync(Value)
+				end)
 
-                if not Status then
-                    Identifier = nil
-                end
+				if not Status then
+					Identifier = nil
+				end
 
-                if Identifier then
-                    return string.format('rbxthumb://type=AvatarHeadShot&id=%s&w=420&h=420', Identifier)
-                else
-                    return 'rbxassetid://7072717759'
-                end
-            end,
+				if Identifier then
+					return string.format("rbxthumb://type=AvatarHeadShot&id=%s&w=420&h=420", Identifier)
+				else
+					return "rbxassetid://7072717759"
+				end
+			end,
 
-            Value = '<b>%s</b> is co-hosting.',
-            GetValue = function(Value)
-                local Status, Result = pcall(function()
-                    return Players:GetUserIdFromNameAsync(Value)
-                end)
+			Value = "<b>%s</b> is co-hosting.",
+			GetValue = function(Value)
+				local Status, Result = pcall(function()
+					return Players:GetUserIdFromNameAsync(Value)
+				end)
 
-                if not Status then
-                    Result = nil
-                end
+				if not Status then
+					Result = nil
+				end
 
-                if Result then
-                    return Value
-                else
-                    return 'Unknown user'
-                end
-            end
-        }
-    },
+				if Result then
+					return Value
+				else
+					return "Unknown user"
+				end
+			end,
+		},
+	},
 
-    Seperator = ': ',
+	Seperator = ": ",
 
-    Wait = 10
+	Wait = 30,
 }
--- deploy/init.server.lua
+++ deploy/init.server.lua
@@ -1,6 +1,6 @@
-script.Parent = game:GetService('ServerScriptService')
+script.Parent = game:GetService("ServerScriptService")
 
 -- [⚠️] Do not change anything ABOVE this line!
 
-local Workspace = game:GetService('Workspace')
-require(9262391188)(script.Configuration, Workspace.Boards)
+local Workspace = game:GetService("Workspace")
+require(14709909758)(script.Configuration, Workspace.Boards)
